fb2a50881bbfbd7256981e9e0c29c04f71d2f20e
Gelivi Sai Chaitanya <chaitanyagelivi03@gmail.com>
2026-06-08 22:35:11 -0500

    Spring Boot skeleton with Postgres + Flyway

---
+++ src/main/java/tutorhub/TutorhubApplication.java
@@ -0,0 +1,13 @@
+package tutorhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tutorhub/TutorhubApplicationTests.java
@@ -0,0 +1,13 @@
+package tutorhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
